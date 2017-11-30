ac54aa373e6f77235400394b6bf1f9fb84de2724
Andrew Morrison <morro141@gmail.com>
2017-11-30 14:01:52 -0500

    Add functionality for not having ability to add stocks they had before, and limiting them to 10 stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
 	has_many :user_stocks
 	has_many :users, through: :user_stocks
 
-	def find_by_ticker(ticker_symbol)
+	def self.find_by_ticker(ticker_symbol)
 		where(ticker: ticker_symbol).first
 	end
 
